004bf71b3bbf4070c54f45980605d6feebdb1132
DayOneAI <bambathiam88@gmail.com>
2023-08-15 00:30:49 +0200

    Final_end

--- src/Title.jsx
+++ src/Title.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const Title = ({ text }) => {
   return (
     <div className="title">
+      {/* Title à supprimer si besoin */}
+      <h1 className="titleBamba">"BAMBA - React Menu template"</h1>
       <h2>{text || 'Default Title'}</h2>
       <div className="title-underline"></div>
     </div>
